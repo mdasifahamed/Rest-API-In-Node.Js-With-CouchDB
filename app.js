5d8a395a7687914f7120972828279dd724989da5
mdasifahamed <mdasifamahed89@yahoo.com>
2024-04-26 02:13:55 +0600

    minimal version final

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(express.static('upload'))
 app.use(fileUploader())
 app.use(bodyParser.json())
 
-// Method To Create Docs 
+
+
+
+// EndPoint  To Create Docs 
 
 app.post("/api/create/", async(req,res)=>{
 
@@ -29,7 +32,6 @@ app.post("/api/create/", async(req,res)=>{
         res.send(result)
     
     } else {
-
         let id = req.body.id; // Id For Counch DB  Unique Id
         let file = req.files.image // extracting the tih the use "express-fileupload"
 
@@ -93,6 +95,122 @@ app.get('/api/media/view/:id',async(req,res)=>{
     })
 })
 
+
+// Api Update Docs 
+
+app.put('/api/doc/single/update/:id', async(req,res)=>{
+
+    await db.get(req.params.id,async (err, data)=>{
+        if(err){
+            res.setHeader('Content-Type','application/json')
+            res.send({data: "DB Error",status:"Invalid Id"})
+            return;
+        }
+        else{
+            // Note: We Need Pass Alll Associated Filed With The Doc Other Wise It Will
+            // It Will Only Kep The Given Filed And Deletes The All The Fields
+            if(!req.body.name || !req.body.description) {
+                res.setHeader('Content-Type','application/json')
+                res.send({data: "Submiison Error ",status:"Invalid Submission"})
+            } else {
+                
+                await db.insert({
+                    _id:req.params.id,
+                    _rev:data._rev, 
+                    name: req.body.name,
+                    description:req.body.description,
+                    image_url:data.image_url,
+                    _attachments:data._attachments},(err)=>{
+    
+                        if(err){
+                            
+                            res.setHeader('Content-Type','application/json')
+                            res.send({data: "DB Error",status:"Failed To Update"})
+                        }
+                        else{
+    
+                            res.setHeader('Content-Type','application/json')
+                            res.send({data: "Updated",status:"Okay",live_url: `http://127.0.0.1:5000/api/doc/single/${req.params.id}`})
+                        }
+                });
+
+
+            }
+            
+        }
+    })
+})
+
+// Add Attachment To Existing Doc 
+
+app.put('/api/media/update/existing/attachment/:id', async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ data: "Error", status: "Invalid Submission" });
+        return;
+    }
+
+    try {
+        const check = await db.get(req.params.id);
+
+        // Get The First Attachment
+        const attachmentName = Object.keys(check._attachments)[0];
+
+        if (!attachmentName) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send({ data: "DB Error", status: "Doc Does Not Have Any Attachments" });
+            return;
+        }
+
+        // Delete The Existing Attachment
+        await db.attachment.destroy(check._id, attachmentName, { rev: check._rev });
+
+        // Get The Image From The Client - Assuming it's in req.files
+        const image = req.files.image;
+        const updatedDoc = await db.get(req.params.id)
+        // Add The New Attachment To The Doc
+        await db.attachment.insert(check._id, image.name, image.data, image.mimetype, { rev: updatedDoc._rev });
+        // Create Media Url For The New Image
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ data: "Done", status: "Successfully Updated The Attachment" });
+
+    } catch (err) {
+        console.error(err);
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ data: "DB Error", status: "Failed To Update The Attachment" });
+    }
+});
+
+// Add Attachments To The Exiting Doc 
+
+
+app.put('/api/media/add/attachmnets/:id', async(req,res)=>{
+
+    if(!req.files){
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ data: "Error", status: "Invalid Submission" });
+        return;
+    }
+
+    try {
+
+        const doc = await db.get(req.params.id);
+        const image  = req.files.image;
+
+        await db.attachment.insert(doc._id, image.name, image.data, image.mimetype, {rev:doc._rev})
+
+        res.setHeader('Content-Type', 'application/json')
+        res.send({data:"done", status:"succesFully Added the Doc"})
+        return;
+    } catch(err) {
+        res.setHeader('Content-Type', 'application/json')
+        res.send({data:"Db Eroor", status:"Failed To Updated"})
+        return;
+    }
+
+})
+
 app.get('/api/doc/list',async(req,res)=>{
     //{include_docs: true} to return the the docs otherwise 
     // only id,key, and _rev will be returned
@@ -109,41 +227,65 @@ app.get('/api/doc/list',async(req,res)=>{
     
 })
 
-// Api Update Docs 
+app.delete('/api/doc/remove/:id', async(req,res)=>{
+    try {
+        const doc = await db.get(req.params.id)
 
-app.put('/api/doc/single/update/:id', async(req,res)=>{
-
-    await db.get(req.params.id,async (err, data)=>{
-        if(err){
-            res.setHeader('Content-Type','application/json')
-            res.send({data: "DB Error",status:"Invalid Id"})
-        }
-        else{
-            // Note: We Need Pass Alll Associated Filed With The Doc Other Wise It Will
-            // It Will Only Kep The Given Filed And Deletes The All The Fields
-            await db.insert({
-                _id:req.params.id,
-                _rev:data._rev, 
-                name: req.body.name,
-                description:req.body.description,
-                image_url:data.image_url,
-                _attachments:data._attachments},(err)=>{
-
-                    if(err){
-                        
-                        res.setHeader('Content-Type','application/json')
-                        res.send({data: "DB Error",status:"Failed To Update"})
-                    }
-                    else{
-
-                        res.setHeader('Content-Type','application/json')
-                        res.send({data: "Updated",status:"Okay",live_url: `http://127.0.0.1:5000/api/doc/single/${req.params.id}`})
-                    }
-                });
-            
-        }
-    })
+        await db.destroy(doc._id,doc._rev)
+        res.setHeader('Content-Type','application/json')
+        res.send({data: "Done",status:"Success Fully Deleted"})
+    } catch (error) {
+        console.log(error)
+        res.setHeader('Content-Type','application/json')
+        res.send({data: "DB Error",status:"failed"})
+    }
 })
+
+
+// Get All The Attchments Url Of An Doc
+
+app.get('/api/media/all/:id',async(req,res)=>{
+    try {
+        const doc = await db.get(req.params.id)
+    if(doc._attachments === undefined){
+        res.setHeader('Content-Type','application/json')
+        res.send({data: "DB Error",status:"Does Not Contain Any Documnetations"})
+        return;
+    }else {
+        const attchtments = Object.keys(doc._attachments)
+        let urlList = []
+        attchtments.map((el,index)=>{
+            let url = `http://127.0.0.1:5000/api/media/view/${doc._id}/${index}`
+                urlList.push(url)
+            });
+        res.setHeader('Content-Type','application/json')
+        res.send({data: urlList,status:"All The Attachmnet Url Is Sent"})
+    }
+    } catch (error) {
+        res.setHeader('Content-Type','application/json')
+        res.send({data: 'Db Error', status:"Invalid ID"})
+    }
+    
+    
+});
+
+// display invividual Attachemnts From Along With Attachment Names
+
+app.get('/api/media/view/:id/:attachment', async(req,res)=>{
+    try {
+        const doc = await db.get(req.params.id)
+        const attachmentName = Object.keys(doc._attachments)[req.params.attachment]
+        const attachmentObject = doc._attachments[attachmentName]
+        res.setHeader('Content-Type', attachmentObject.content_type)
+        db.attachment.getAsStream(req.params.id,attachmentName).pipe(res)
+    } catch (error) {
+        console.log(error);
+        res.setHeader('Content-Type','application/json')
+        res.send({data:'Db Erro', status:"Failed Display"})
+
+    }
+})
+
 
 
 
