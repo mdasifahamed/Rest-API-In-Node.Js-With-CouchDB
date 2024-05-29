28447c44d9943e124ac5ee24d6ac94e3c931c4ab
mdasifahamed <mdasifahamed89@yahoo.com>
2024-05-29 15:45:04 +0600

    Note for when to use  and  in the db url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dbpass= process.env.DBPASS
 const express = require('express')
 const bodyParser =  require('body-parser')
 const fileUploader = require('express-fileupload')
-const nano = require('nano')(`http://${dbuser}:${dbpass}@couchdb:5984`)
+const nano = require('nano')(`http://${dbuser}:${dbpass}@couchdb:5984`) // To run the app in the locally cahnge the `cocuhdb` to `localhost` 
 const app = express()
 const db = nano.use('resapi')
 app.use(express.static('upload'))
